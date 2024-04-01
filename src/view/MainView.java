4d30621f56df80bf895e8dc787741f37b7fbd288
Kee Qing <chankeeqing0303@gmail.com>
2024-04-01 21:53:14 +0800

    Added MainView

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -1,5 +1,34 @@
 package view;
 
 public abstract class MainView {
-	abstract public void printView();
+    /**
+     * Abstract method for view menu
+     */
+    protected abstract void printActions();
+
+    /**
+     * Abstract method for view app
+     */
+    public abstract void viewApp();
+
+    /**
+     * Default constructor for main view
+     */
+    public MainView() {
+  
+    }
+
+    /**
+     * Method to print breadcrumbs for navigation purposes
+     * @param breadcrumb breadcumbs description
+     */
+    protected void printBreadCrumbs(String breadcrumb) {
+        String spaces = String.format("%" + (105 - breadcrumb.length()) + "s", "");
+        System.out.println(
+                "╔══════════════════════════════════════════════════════════════════════════════════════════════════════════╗");
+        System.out.println("║ " + breadcrumb + spaces + "║");
+        System.out.println(
+                "╚══════════════════════════════════════════════════════════════════════════════════════════════════════════╝");
+    }
+    
 }
