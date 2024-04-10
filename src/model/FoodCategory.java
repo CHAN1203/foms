a367b65b19da9d86f9f460d9f47b98983a70278a
Jacky Lim <limjacky318@outlook.com>
2024-04-10 21:22:04 +0800

    editted FoodCategory model

--- src/model/FoodCategory.java
+++ src/model/FoodCategory.java
@@ -1,7 +1,20 @@
 package model;
 
-import enums.FoodCategory;
-
 public class FoodCategory {
-
+	private String name;
+	
+	public FoodCategory(String name) {
+		setFoodCategory(name);
+	}
+	
+	public boolean setFoodCategory(String name) {
+		this.name = name;
+		return true;
+	}
+	
+	public String getFoodCategory(String name) {
+		return name;
+	}
+	
+	
 }
