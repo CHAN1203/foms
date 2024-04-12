9227ceae870024bdbdb5a6f3f48d7e8ad22c36e1
lyh0805 <euhanglee@gmail.com>
2024-04-13 01:29:54 +0800

    Update Repository.java

--- src/repository/Repository.java
+++ src/repository/Repository.java
@@ -16,8 +16,10 @@ import java.io.ObjectOutputStream;
 public class Repository {
     private static final String folder = "data";
     
-    public static HashMap<String, Employee> EMPLOYEE = new HashMap<String, Employee>();
-    public static HashMap<String, Branch> BRANCH = new HashMap<String, Branch>();
+    public static HashMap<String, Employee> EMPLOYEE = new HashMap<>();
+    public static HashMap<String, Branch> BRANCH = new HashMap<>();
+    public static HashMap<Integer, Order> ORDER = new HashMap<>(); // <orderID, Order>
+    
     //haven't written any serialization part
     public static Set<String> BRANCHES = new HashSet<>();
     public static Set<String> PAYMENT_METHODS = new HashSet<>();
