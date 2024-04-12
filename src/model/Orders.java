00f9a4f3df6dd62725a0cb12294c8f623fba1837
lyh0805 <153891763+lyh0805@users.noreply.github.com>
2024-04-12 21:59:54 +0800

    Update Orders.java

--- src/model/Orders.java
+++ src/model/Orders.java
@@ -4,11 +4,25 @@ public class Orders {
 	private int orderID;
 	private String status;
 
-	public void setOrderID(){
-		this.OrderID = OrderID;
+	public void vOrders(int orderID, String status){
+		this.orderID = orderID;
+		this.status = status;
 	}
 
-	public void set status(){
+	public bool setOrderID(int orderID){
+		
+		if (orderID > 0) {
+			this.orderID = orderID;
+			return True;
+		}
+
+		else {
+			return False;
+		}
+	}
+
+	public bool setStatus(String status){
 		this.status = status;
+		return True;
 	}
 }
